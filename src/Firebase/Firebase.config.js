6a78f02a059fc3893143c12f41d01f53d362240c
Tanvir Rahman Rafi <67752189+Tanvir19026@users.noreply.github.com>
2024-05-26 19:24:44 +0600

    Update Firebase.config.js

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -2,12 +2,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAX1M5vmiPzufS1k7GXd_Io-W9ObtQlESk",
-  authDomain: "movieworld-72738.firebaseapp.com",
-  projectId: "movieworld-72738",
-  storageBucket: "movieworld-72738.appspot.com",
-  messagingSenderId: "557500706428",
-  appId: "1:557500706428:web:a251d33a6294623693a3f9"
+  
 };
 
 
