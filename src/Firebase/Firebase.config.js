ed13c96cec339bd7eb0c8926d309ba96526e3f2e
Tanvir Rahman Rafi <67752189+Tanvir19026@users.noreply.github.com>
2024-05-26 19:26:37 +0600

    Update Firebase.config.js

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -2,7 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  
+  apiKey:Vite_apikey,
+  authDomain: vite_authDomain,
+  projectId: vite_projectId,
+  storageBucket: vite_storageBucket,
+  messagingSenderId:vite_messagingSenderId,
+  appId: vite_appId
 };
 
 
